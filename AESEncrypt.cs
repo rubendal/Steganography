9e7f7989051ced9224db85b8d5b4d4e6a0beae51
rubendal <rubendal@users.noreply.github.com>
2015-09-16 19:40:36 -0500

    Removed comments and unnecessary random string

--- AESEncrypt.cs
+++ AESEncrypt.cs
@@ -10,25 +10,17 @@ namespace Steganography
 {
     public class AESEncrypt
     {
-        private static string IV = "icD0?Ynl3HU8!/nkW##p"; //some random string
 
         static AesCryptoServiceProvider CreateAES(string key)
         {
             byte[] bytes = Encoding.ASCII.GetBytes(key);
             AesCryptoServiceProvider a = new AesCryptoServiceProvider();
-            //a.BlockSize = 128;
-            //a.KeySize = 256;
             Rfc2898DeriveBytes r = new Rfc2898DeriveBytes(key, new byte[] { 1,2,3,4,5,6,7,8});
             a.Key = r.GetBytes(a.BlockSize / 8);
             a.IV = new byte[a.BlockSize / 8];
             a.Padding = PaddingMode.PKCS7;
             a.Mode = CipherMode.CBC;
             return a;
-            //MD5 md5 = new MD5CryptoServiceProvider();
-            //TripleDES des = new TripleDESCryptoServiceProvider();
-            //des.Key = md5.ComputeHash(Encoding.Unicode.GetBytes(key));
-            //des.IV = new byte[des.BlockSize / 8];
-            //return des;
         }
 
         public string EncryptString(string text, string password)
